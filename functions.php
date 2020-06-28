8147c98d3429610f1743ce160a4ab07a8424cdf6
pixelstorm <dan@pixelstorm.com.au>
2020-06-28 12:48:46 +1000

    add footer menu

--- functions.php
+++ functions.php
@@ -84,7 +84,8 @@ class StarterSite extends Timber\Site {
 		$context['foo']   = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
+		$context['menu']  = new Timber\Menu(16);
+		$context['footer_menu']  = new Timber\Menu(19);
 		$context['site']  = $this;
 		//$context['average_reading_time']  = get_field('average_reading_time', 'options');
 		//$context['header_banner_text'] = get_field('breaking_label', 'options');
